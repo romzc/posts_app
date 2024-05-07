ab9e25fed1cd692c2d6bab5b9ce5e0c42615d05a
romzc <ronyramos495@gmail.com>
2024-05-07 06:26:10 -0500

    se agrego la funcionalidad de login y authorización

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='users'
 
 urlpatterns = [
-  path('register/', views.user_register, name='register')
+  path('register/', views.user_register, name='register'),
+  path('login/', views.user_login, name='login'),
+  path('logout/', views.user_logout, name='logout'),
 ]
